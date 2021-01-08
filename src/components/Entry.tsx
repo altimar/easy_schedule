3ccc5a620f794d63082036e954489d89812f1293
Pavel Ivantsov <pavel.ivantsov@semrush.com>
2021-01-08 15:44:39 +0300

    + add bad_participants list to Entry component + highlight bar participants + highlight bad entries

--- src/components/Entry.tsx
+++ src/components/Entry.tsx
@@ -10,26 +10,34 @@ interface IProps {
   index: number,
   title: string,
   participants?: string[],
+  bad_participants?: string[],
   onClick?: () => void,
 }
 
-export default function Entry(props:IProps) {
-    return (
-      <Draggable draggableId={props.entry_id.toString()} index={props.index}>
-        {(provided) => (
-          <Card className="Entry" onClick={props.onClick} {...provided.draggableProps} {...provided.dragHandleProps} ref={provided.innerRef}>
-            <CardContent>
-              <div className="Entry-title">{props.title}</div>
-              <div className="Entry-participant-list">
-                {
-                  props.participants && props.participants.map(participant => {
-                    return <Chip label={participant} color="primary" key={participant} />;
-                  })
-                }
-              </div>
-            </CardContent>
-          </Card>
-        )}
-      </Draggable>
-    )
+export default function Entry(props: IProps) {
+  let bad_participants: { [id: string]: boolean } = {};
+  let style = {};
+  if (props.bad_participants && props.bad_participants.length) {
+    props.bad_participants.forEach((item: string) => { bad_participants[item] = true })
+    style = { backgroundColor: "#fdd" };
+  }
+  return (
+    <Draggable draggableId={props.entry_id.toString()} index={props.index}>
+      {(provided) => (
+        <Card className="Entry" onClick={props.onClick} {...provided.draggableProps} {...provided.dragHandleProps} ref={provided.innerRef}>
+          <CardContent style={style}>
+            <div className="Entry-title">{props.title}</div>
+            <div className="Entry-participant-list">
+              {
+                props.participants && props.participants.map(participant => {
+                  const color = bad_participants[participant] ? "secondary" : "primary";
+                  return <Chip label={participant} color={color} key={participant} />;
+                })
+              }
+            </div>
+          </CardContent>
+        </Card>
+      )}
+    </Draggable>
+  )
 }
